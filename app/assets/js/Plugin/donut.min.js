da85d949fe80a14c08559b4f24beebc35b240ac2
Chris Rocco <chris.rocco7@gmail.com>
2017-07-07 23:47:13 -0500

    angular

--- app/assets/js/Plugin/donut.min.js
+++ app/assets/js/Plugin/donut.min.js
@@ -1,5 +1,5 @@
 /*!
- * bigdata (https://uab-energetics.github.io/bigdata-app/html/)
+ * tcc-app (https://github.com/vector-web-development/tcc-client)
  * Copyright 2017 chrisrocco
  * Licensed under the Themeforest Standard Licenses
  */
