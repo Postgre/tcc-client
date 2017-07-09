1787c329756536f2ca7969e71fa4ee3066ed6fbe
Chris Rocco <chris.rocco7@gmail.com>
2017-07-08 22:07:06 -0500

    She's ready for you, Andrew

--- shared/angular/booking/booking.controller.js
+++ shared/angular/booking/booking.controller.js
@@ -2,13 +2,16 @@ angular.module('booking')
 .controller('BookingController', BookingController);
 
 function BookingController( $scope ) {
-    init();
 
     $scope.event = DEFAULT_MODEL.event;
-    $scope.selectedMarket = getMarketById( DEFAULT_MODEL.event.market_id );
+    $scope.selectedMarket = DEFAULT_MODEL.market;
+    $scope.invoice = DEFAULT_MODEL.invoice;
+    $scope.travel_costs = DEFAULT_MODEL.travel_costs;
     $scope.change = function(){
         console.log( $scope.event );
-    }
+    };
+
+    init();
     /**
      * Models
      * ===============
@@ -24,15 +27,6 @@ function BookingController( $scope ) {
      * ===============
      */
     function init(){
-        $('.typeahead').typeahead({
-                hint: true,
-                highlight: true,
-                minLength: 1
-            },
-            {
-                name: 'states',
-                source: substringMatcher(MOCK_MARKETS)
-            });
         $(".daterange").daterangepicker({
             "opens": "center",
             timePicker: true,
@@ -49,37 +43,9 @@ function BookingController( $scope ) {
                 $scope.event.end_time = picker.endDate.format("MM/DD/YYYY h:mm A");
             });
         });
+        window.market = $scope.selectedMarket;
     }
 }
-
-function getMarketById( id ){
-    for( var i = 0; i < MOCK_MARKETS.length; i++ ){
-        var m = MOCK_MARKETS[i];
-        if(m.id == id) return m;
-    }
-}
-
-var substringMatcher = function(markets) {
-    return function findMatches(q, cb) {
-        var matches, substringRegex;
-
-        // an array that will be populated with substring matches
-        matches = [];
-
-        // regex used to determine if a string contains the substring `q`
-        substrRegex = new RegExp(q, 'i');
-
-        // iterate through the pool of strings and for any string that
-        // contains the substring `q`, add it to the `matches` array
-        $.each(markets, function(i, mkt) {
-            if (substrRegex.test(mkt.name)) {
-                matches.push(mkt.name);
-            }
-        });
-
-        cb(matches);
-    };
-};
 
 const DEFAULT_MODEL = {
     event: {
@@ -92,18 +58,28 @@ const DEFAULT_MODEL = {
         start_time: "07/19/2017 10:00 AM",
         end_time: "07/19/2017 12:00 AM",
         details: [],
-        special_requests: "White carolers only please",
-        market_id: 123
+        special_requests: "Send midgets if you have any. Please, thanks."
+    },
+    market: {
+        name: "The Birmingham Carolers",
+        description: "Beautiful Birmingham is known for its impressive skyline. That skyline lights up even brighter at Christmas, when holiday lights adorn every corner. From the Zoolight Safari at Birmingham Zoo to the city tree lighting ceremony, there’s a lot to love.",
+        address: "Birmingham, AL",
+        banner: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BirminghamALskyline2.jpg/900px-BirminghamALskyline2.jpg"
+    },
+    invoice: {
+        carolers: 400,
+        travel: 43,
+        parking: 0,
+        toll_roads: 0,
+        special_dates: 0,
+        promos: -20,
+        total: 423
+    },
+    travel_costs: {
+        miles: 32,      // miles
+        time: 44,       // minutes
+        miles_fee: 8,   // USD
+        time_fee: 28,   // USD
+        tolls: 6        // USD
     }
 };
-
-const MOCK_MARKETS = [
-    {
-        id: 123,
-        name: "The Birmingham Carolers"
-    },
-    {
-        id: 456,
-        name: "The New York Carolers"
-    }
-];
