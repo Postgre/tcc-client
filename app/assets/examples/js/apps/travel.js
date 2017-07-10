7a2ad4aab000b686ce88f7060957cbbbaf83d653
Chris Rocco <chris.rocco7@gmail.com>
2017-07-10 01:12:53 -0500

    DELETED SECOND TEMPLATE ENVIORNMENT. Looks like 'Canvas' has everything we need!

--- app/assets/examples/js/apps/travel.js
+++
@@ -1,8 +0,0 @@
-/*!
- * tcc-app (https://github.com/vector-web-development/tcc-client)
- * Copyright 2017 chrisrocco
- * Licensed under the Themeforest Standard Licenses
- */
-$(document).ready(function() {
-  AppTravel.run();
-});
