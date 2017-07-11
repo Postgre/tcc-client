afb610c2bd082ec4d7b9252433ce945214c49503
Chris Rocco <chris.rocco7@gmail.com>
2017-07-10 22:01:12 -0500

    published vs not

--- shared/angular/market-edit/market-edit.controller.js
+++ shared/angular/market-edit/market-edit.controller.js
@@ -15,6 +15,8 @@ function MarketEditController( $scope ) {
     $scope.deleteSpecialDate = deleteSpecialDate;
     $scope.editSpecialDates = editSpecialDates;
     $scope.stopEditingSpecialDates = stopEditingSpecialDates;
+    $scope.publishMarket = publishMarket;
+    $scope.unpublishMarket = unpublishMarket;
 
     function deleteSpecialDate( date ){
         var ind = $scope.market.specialDates.indexOf( date );
@@ -27,6 +29,45 @@ function MarketEditController( $scope ) {
     function stopEditingSpecialDates() {
         $scope.editClassMap = "col-sm-6";
         $scope.editing_sd = false;
+    }
+    function publishMarket() {
+        swal({
+            title: "Lets go Live!",
+            text: "Once this market is published, it will be discoverable and users can book events. Please review the market settings before publishing!",
+            type: "info",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Ok, publish market!",
+            closeOnConfirm: false,
+            html: false
+        }, function(){
+            $scope.$apply(function(){
+                $scope.market.published = true;
+            });
+            $scope.market.published = true;
+            swal("We're Live!",
+                $scope.market.name + " is live and discoverable!",
+                "success");
+        });
+    }
+    function unpublishMarket() {
+        swal({
+            title: "Take Down Market?",
+            text: "You are about to un-publish this market. This will make it non-discoverable, but not affect existing events.",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Un-publish Market!",
+            closeOnConfirm: false,
+            html: false
+        }, function(){
+            $scope.$apply(function(){
+                $scope.market.published = false;
+            });
+            swal("Done.",
+                $scope.market.name + " is no longer discoverable",
+                "success");
+        });
     }
 
     function init(){
@@ -45,6 +86,7 @@ const DEFAULT_MODEL = {
         address: "1617 13th Avenue South",
         email: "chris.rocco7@gmail.com",
         phone: 2056396666,
+        published: false,
         video: "https://www.youtube.com/watch?v=DA2QpqgIq6g",
         specialDates: [ {} ],
         image: "http://soulofamerica.com/soagalleries/birmingham/enjoy/Birmingham-skyline.jpg",
