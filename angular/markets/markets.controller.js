540c28728b6124ea5237d7a0a102ea16f3d73e72
Chris Rocco <chris.rocco7@gmail.com>
2017-07-15 15:58:49 -0500

    market page little fix

--- angular/markets/markets.controller.js
+++ angular/markets/markets.controller.js
@@ -99,7 +99,7 @@ const DEFAULT_MODEL = {
             start_time: "07/19/2017 10:00 AM",
             end_time: "07/19/2017 12:00 AM",
             details: [],
-            description: "We'll have a jolly-good time.",
+            description: "Bring your own egg-nog",
             special_requests: "Send midgets if you have any. Please, thanks.",
             image: "http://www.blogingbloging.com/wp-content/uploads/2013/08/christmas-party.jpg",
             status: "Not Paid"
