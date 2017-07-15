594eb6336f6309dd6ffd41527a1eba127fc82ae0
Chris Rocco <chris.rocco7@gmail.com>
2017-07-15 00:18:01 -0500

    dataservice

--- es/src/services/DataService.js
+++ es/src/services/DataService.js
@@ -1,8 +1,26 @@
+const axios = require('axios');
+
 /* Data Service */
-module.exports = class DataService{
-  constructor(config, AuthService){
-    console.info('DataService Loading...');
-  }
+module.exports = class DataService {
+    constructor(config, authService) {
+        console.info('DataService Loading...');
 
+        /* Connection to server */
+        this.connection = axios.create({
+            baseURL: config.serverURL,
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + authService.jwt
+            }
+        })
+    }
 
+    getProtected(){
+        var p = this.connection({
+            url: "/protected",
+        });
+        p.then( (res)=>{
+            console.log( "response", res );
+        });
+    }
 };
