54073866d055ab271352390224014397d0c16a31
Chris Rocco <chris.rocco7@gmail.com>
2017-07-15 17:43:19 -0500

    crazy effective build tool

--- scripts/RoccoBuild.js
+++ scripts/RoccoBuild.js
@@ -1,36 +1,34 @@
+const fs = require('fs');
+const glob = require("glob");
+const cheerio = require('cheerio'); // https://github.com/cheeriojs/cheerio
+
 module.exports = class RoccoBuild {
     constructor(){
         this.operations = [];
         this.templates = {};
     }
 
-    operate( file_path, operations ){
+    // 1.)
+    registerOperation( operation ){
+        this.operations.push( operation );
+    }
+    // 2.)
+    executeOperations( on_directory ){
+        const self = this;
+        glob(on_directory+"*.html", function (er, files) {
+            for (let i = 0; i < files.length; i++) {
+                let file = files[i];
+                RoccoBuild.operate( file, self.operations);
+            }
+        });
+    }
+
+    static operate( file_path, operations ){
         let page = cheerio.load(fs.readFileSync(file_path, 'UTF-8'));
         for (let i = 0; i < operations.length; i++) {
             let op = operations[i];
             op(page);
         }
         fs.writeFileSync(file_path, page.html());
-    }
-
-    executeOperations( on_directory ){
-        glob(on_directory+"*.html", function (er, files) {
-            for (let i = 0; i < files.length; i++) {
-                let file = files[i];
-                this.operate( file, this.operations);
-            }
-        });
-    }
-
-    registerOperation( operation ){
-        this.operations.push( operation );
-    }
-
-    registerTemplate( name, html ){
-        this.templates.name = html;
-    }
-
-    tpl( name ){
-        return this.templates[name];
     }
 };
--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,25 @@
-const replace = require("replace");
 const fs = require('fs');
 const glob = require("glob");
 const cheerio = require('cheerio'); // https://github.com/cheeriojs/cheerio
-const RoccoBuild = require('./RoccoBuild');
 
+// Load library
+const RoccoBuild = require('./RoccoBuild');
 let roccoBuild = new RoccoBuild();
 
-/**
- * Load Assets
- * ===================
- */
+// Load static assets/templates
 const header = fs.readFileSync('./partials/header.htm', 'UTF-8');
 const footer = fs.readFileSync('./partials/footer.htm', 'UTF-8');
 
+// Define the sync operations
 roccoBuild.registerOperation( function($){
     $('header').html(header);
 });
 roccoBuild.registerOperation( function($){
     $('footer').replaceWith(footer);
 });
+roccoBuild.registerOperation( function($){
+    $('angular').replaceWith('<script src="node_modules/angular/angular.min.js"></script>');
+});
 
+// Execute them on the project root
 roccoBuild.executeOperations('./');
