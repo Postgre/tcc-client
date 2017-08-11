38a970bfb02acdbaf1922eae14cfa8fcd1c9613d
Chris Rocco <chris.rocco7@gmail.com>
2017-08-10 23:05:37 -0500

    bad address when creating market

--- app/markets-manage/markets-manage.controller.js
+++ app/markets-manage/markets-manage.controller.js
@@ -4,11 +4,7 @@ angular.module('markets-manage')
 function MarketsManageController( $scope ) {
 
     $scope.markets = [];
-    $scope.form = {
-        name: "TEST",
-        state: "MS",
-        city: "Biloxi"
-    };
+    $scope.form = {};
 
     function init(){
         window.dataService.getMarketsManaged().then(function(_markets){
@@ -39,77 +35,19 @@ function MarketsManageController( $scope ) {
      */
     $scope.create = () => {
         let market = modelFactory.create("Market", parseNewMarketForm());
-        market.save().then(onCreate).catch(somethingWentWrong);
+        market.save().then(onCreate).catch((err)=>{
+            let s = err.response.data.status;
+            if(s === "BAD_ADDRESS"){
+                alertBadAddress();
+                return;
+            }
+            somethingWentWrong();
+        });
     };
     $scope.destroy = (market) => {
         market.destroy().then(onDelete).catch(somethingWentWrong);
     };
-
-    // $scope.createMarket         = function(){
-    //     let formData = parseNewMarketForm();
-    //     window.dataService.postResource("markets", {
-    //             name: formData.name,
-    //             bio: formData.bio,
-    //             address: formData.address
-    //         }).then(function(id){
-    //             console.info("res", id);
-    //             swal({
-    //                 title: "Success!",
-    //                 text: "'"+formData.name+"' has been created!",
-    //                 type: "success",
-    //             }, function(){
-    //                 init();
-    //             });
-    //         }).catch(function(err){
-    //             console.error("err", err);
-    //             sweetAlert("Oops...", "Something went wrong!", "error");
-    //         });
-    // };
-
-    // $scope.deleteMarket         = function(market){
-    //     let win = () => {
-    //
-    //     };
-    //     let doIt = () => {
-    //         let p = window.dataService.deleteResource("markets", market.id);
-    //         p.then( win );
-    //         p.catch(window.somethingWentWrong);
-    //     };
-    //
-    //     swal({
-    //         title: "Delete Market?",
-    //         text: "Are you sure? This can't be undone.",
-    //         type: "warning",
-    //         showCancelButton: true,
-    //         confirmButtonColor: "#DD6B55",
-    //         confirmButtonText: "Yes, delete it!",
-    //         closeOnConfirm: false
-    //     }, doIt );
-    // };
-
     $scope.navService = window.navService;
-
-    $scope.gotoEdit             = function(market){
-        window.navService.goto("edit_market", {
-            market_id: market.id,
-            market: market
-        })
-    };
-    $scope.gotoPage             = function(market){
-        window.navService.goto('market_page', {
-            market_id: market.id
-        })
-    };
-    $scope.gotoUpcomingEvents   = function(market){
-        window.navService.goto('upcoming_events', {
-            market: market
-        });
-    };
-    $scope.gotoInvites          = function(market){
-        window.navService.goto("invites", {
-            market: market
-        });
-    };
 
     function parseNewMarketForm() {
         $scope.form['address'] = `${$scope.form.city}, ${$scope.form.state}`;
@@ -117,4 +55,9 @@ function MarketsManageController( $scope ) {
     }
 
     init();
+}
+
+
+function alertBadAddress(){
+    swal("Bad Address", "That address could not be resolved", "error");
 }
