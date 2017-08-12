caec496f56464703db6d1bb86c7c51aaa23f582c
Chris Rocco <chris.rocco7@gmail.com>
2017-08-11 20:32:21 -0500

    DONE: redeem caroler invite code

---
+++ redeem-invite.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+    <?php require 'partials/head.htm' ?>
+    <?php require 'partials/angular.html' ?>
+
+    <!-- CSS
+    ================================= -->
+<!--    <link href="css/custom.css" rel="stylesheet" />-->
+</head>
+<body class="stretched">
+<div id="wrapper" class="clearfix">
+    <?php require 'partials/top-bar.htm' ?>
+    <?php require 'partials/header-2.php' ?>
+    <?php require 'app/redeem-invite/redeem-invite.html' ?>
+    <?php require 'partials/footer.htm' ?>
+</div>
+
+<!-- JAVASCRIPTS
+========================================= -->
+<?php require 'partials/javascripts.htm' ?>
+</body>
+</html>
