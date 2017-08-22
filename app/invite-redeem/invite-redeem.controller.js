2f4b0c8193ddf5e751d5abd8c0e37ef28048df3a
Chris Rocco <chris.rocco7@gmail.com>
2017-08-21 20:20:14 -0500

    hotfix

--- app/invite-redeem/invite-redeem.controller.js
+++ app/invite-redeem/invite-redeem.controller.js
@@ -47,6 +47,7 @@ function InviteRedeemController( $scope ){
 
 function onWin(){
     swal("Success!", "You have joined this market!", "success");
+    authService.logout();
 }
 function onDupes(){
     swal("Wait a Minute!", "You're already in this market", "warning");
