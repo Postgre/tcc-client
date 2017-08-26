5144c037da83e28bf0749f76cf0ada53842b95f4
Chris Rocco <chris.rocco7@gmail.com>
2017-08-26 16:17:58 -0500

    event single page

--- app/customer-events/customer-events.controller.js
+++ app/customer-events/customer-events.controller.js
@@ -21,20 +21,42 @@ function CustomerEventsController( $scope ) {
      */
     function init(){
         loadCalendar({});
-        dataService.getMyEvents().then(
-            (events)=>{
-                let calData = {};
-                events.forEach(function(event){
-                    // loading into calendar format
-                    let date = moment(event.start_time);
-                    event.startDate = date.format("D");
-                    event.startMonth = date.format("MMM");
-                    calData[date.format("MM-DD-YYYY")] = event.name;
-                });
-                loadCalendar(calData);
-                $scope.upcoming_events = events;
-                $scope.$apply();
-            }
-        );
+
+        function ready(){
+            $scope.$apply();
+        }
+
+        let now = moment().format("YYYY-MM-DD");
+        /* load upcoming events */
+        modelFactory.all("Booking", {
+            where: "end_time > "+now
+        }).then((events)=>{
+            let calData = {};
+            events.forEach(function(event){
+                // loading into calendar format
+                let date = moment(event.start_time);
+                event.startDate = date.format("D");
+                event.startMonth = date.format("MMM");
+                calData[date.format("MM-DD-YYYY")] = event.name;
+            });
+            loadCalendar(calData);
+            $scope.upcoming = events;
+            ready();
+        });
+        // end
+        /* load past events */
+        modelFactory.all("Booking", {
+            where: "end_time < "+now
+        }).then((events)=>{
+            events.forEach(function(event){
+                let date = moment(event.start_time);
+                event.startDate = date.format("D");
+                event.startMonth = date.format("MMM");
+            });
+
+            $scope.past = events;
+            ready();
+        });
+        // end
     }
 }
