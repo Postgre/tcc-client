d29fde66c37e48872fb18f277f6c1fff6f3730e4
Chris Rocco <chris.rocco7@gmail.com>
2017-09-03 22:02:46 -0500

    more event status labels

--- app/shared/components/event/status.directive.js
+++ app/shared/components/event/status.directive.js
@@ -9,6 +9,11 @@ angular.module("components")
             <label ng-if="status==='overdue'" class="label label-danger">Overdue</label>
             <label ng-if="status==='pending_deposit'" class="label label-warning">Pending Deposit</label>
             <label ng-if="status==='pending_payment'" class="label label-warning">Pending Payment</label>
+            <label ng-if="status==='pending_approval'" class="label label-warning">Pending Approval</label>
+            <label ng-if="status==='terminated'" class="label label-default">Terminated</label>
+            <label ng-if="status==='canceled'" class="label label-default">Canceled</label>
+            <label ng-if="status==='past'" class="label label-default">Past</label>
+            <label ng-if="status==='past'" class="label label-default">Past</label>
         `
     }
 });
