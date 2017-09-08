85a52fdcabacd010ed093ff11f41d2d7fdc54cc5
Chris Rocco <chris.rocco7@gmail.com>
2017-09-08 15:23:18 -0500

    just a thing

--- app/shared/components/event/event-details.directive.js
+++ app/shared/components/event/event-details.directive.js
@@ -9,12 +9,9 @@ function tccEventDetails(){
         },
         templateUrl: "/app/shared/components/event/event-details.htm",
         controller: function($scope){
-            let DATETIME_FORMAT = "YYYY-MM-DD HH:MM";
-            let DATE_FORMAT = DATETIME_FORMAT.split(" ")[0];
-            let TIME_FORMAT = DATETIME_FORMAT.split(" ")[1];
-            $scope.date = moment($scope.booking.start_time).format(DATE_FORMAT);
-            $scope.start = moment($scope.booking.start_time).format(TIME_FORMAT);
-            $scope.end = moment($scope.booking.end_time).format(TIME_FORMAT);
+            $scope.fmt = function(date, pattern){
+                return moment(date).format(pattern);
+            }
         }
     };
 }
