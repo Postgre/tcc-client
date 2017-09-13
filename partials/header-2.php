977b4aa340cfa5b612f6ff0b61bfa8851a162e7c
Chris Rocco <chris.rocco7@gmail.com>
2017-09-13 00:13:57 -0500

    .

--- partials/header-2.php
+++ partials/header-2.php
@@ -1,7 +1,8 @@
-<!--[if lt IE 8]>
+<!--[if IE]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
     your browser</a> to improve your experience.</p>
 <![endif]-->
+
 <header id="header" class="full-header dark">
     <?php require __DIR__ . '/header.htm'; ?>
 </header>
