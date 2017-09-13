aae786e6cf9bb6e848232d4672d11a81c7103cf2
Chris Rocco <chris.rocco7@gmail.com>
2017-09-13 00:09:37 -0500

    ie check

--- partials/header-1.php
+++ partials/header-1.php
@@ -1,3 +1,7 @@
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
+<![endif]-->
 <header id="header" class="full-header transparent-header dark" data-sticky-class="dark">
     <?php require __DIR__ . '/header.htm'; ?>
 </header>
